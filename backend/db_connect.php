7256b759006ca66a38f39a88f806d89d2efd8457
msofiee <angelasofiaalmuena.salimpade24@bicol-u.edu.ph>
2026-05-13 01:52:48 +0800

    Added database connection

---
+++ backend/db_connect.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "inventory";
+
+try {
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
